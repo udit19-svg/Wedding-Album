0ef7b1df8ab88b33c36081dfd91c46ca29bb399b
Udit <singerudit19@gmail.com>
2026-04-26 15:32:20 +0530

    new url qr

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -138,7 +138,7 @@ function Upload() {
         <div className="wedding-qr-section wedding-success">
           <h3 className="wedding-qr-title">Scan QR to view album 📱</h3>
         <div className="wedding-qr-wrapper">
-          <QRCodeCanvas value={`https://wedding-album.singerudit19.workers.dev/album/${albumId}`}
+          <QRCodeCanvas value={`https://69dae427.weds-2az.pages.dev/album/${albumId}`}
            size={200}
             level="H"
             includeMargin={true} />  </div>
